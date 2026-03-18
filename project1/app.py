13acfa7426013615f7c8b959a734abe647fbfd53
Olivia Waithira <oliviawaithira7@gmail.com>
2026-03-18 07:02:23 +0300

    form

--- project1/app.py
+++ project1/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    name= request.args.get("name", "World")
     return render_template("index.html")
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "World"))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
